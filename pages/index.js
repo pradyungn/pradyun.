03e603af69611e81df225f3481686e13dc03fa6c
Pradyun Narkadamilli <pradyungn@gmail.com>
2021-11-29 19:58:14 -0800

    added icons to mobile versoin - some rerendering issues

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
+import {
+    FaGithubAlt,
+    FaFacebookF,
+    FaLinkedinIn,
+} from "react-icons/fa"
+
+import { GrMail } from "react-icons/gr"
+
 import React, { useState } from 'react';
 import styles from '../styles/Home.module.css'
 import NavBar from '../components/navbar'
 
 export default function Home() {
   const [theme, setTheme] = useState("light");
+
+  const gh = "gh"
+  const fb = "fb"
+  const li = "li"
+  const em = "em"
+
+  if (typeof window !== 'undefined' && window.innerWidth < 620) {
+      fb = ( <FaFacebookF/> )
+      gh = ( <FaGithubAlt/> )
+      li = ( <FaLinkedinIn/> )
+      em = ( <GrMail/> )
+  }
+
   return (
       <main className={"container " + theme}>
         <NavBar hook={setTheme} curr={theme}/>
@@ -16,17 +37,17 @@ export default function Home() {
             </div>
             <div className={styles.bubbles}>
                 <div className={styles.gh}>
-                    <a target="_blank" href="https://github.com/pradyungn">gh</a>
+                    <a target="_blank" href="https://github.com/pradyungn">{gh}</a>
                 </div>
                 <div className={styles.li}>
-                    <a target="_blank" href="https://linkedin.com/in/pradyun">li</a>
+                    <a target="_blank" href="https://linkedin.com/in/pradyun">{li}</a>
                 </div>
                 <div className={styles.fa}>
                     <a target="_blank" href="https://www.facebook.com/profile.php?id=100009257228843">
-                        fb</a>
+                        {fb}</a>
                 </div>
                 <div className={styles.em}>
-                    <a target="_blank" href="mailto:pradyun2@illinois.edu">em</a>
+                    <a target="_blank" href="mailto:pradyun2@illinois.edu">{em}</a>
                 </div>
             </div>
         </div>
