55bb2442d9fb606f8bf611aadcf1b9708e1eb5af
Pradyun Narkadamilli <pradyungn@gmail.com>
2022-06-05 00:57:45 -0700

    oops

--- components/meta.js
+++ components/meta.js
@@ -5,11 +5,12 @@ export default (props)=> (
     <title>{ props.siteTitle }</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content={ "description" in props ? props.description : "Maker of things. Engineering afficionado. " }/>
-    <meta name="og:description" content={ "description" in props ? props.description : "Maker of things. Engineering afficionado. " }/>
-    <meta name="og:title" content={ props.siteTitle }/>
-    <meta name="og:type" content="website"/>
-    <meta name="og:url" content={`https://pradyun.tech/${"path" in props ? props.path : ""}`}/>
-    <meta name="og:image" content={
+    <meta property="og:description" content={ "description" in props ? props.description : "Maker of things. Engineering afficionado. " }/>
+    <meta property="og:title" content={ props.siteTitle }/>
+    <meta property="og:site_name" content={ props.siteTitle }/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content={`https://pradyun.tech/${"path" in props ? props.path : ""}`}/>
+    <meta property="og:image" content={
       `https://pradyun.vercel.app/${"img" in props ? props.img : "me.webp"}`
     }/>
   </Head>
