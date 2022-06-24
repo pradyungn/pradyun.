5cbac63123dfbcf4a9f9451c07b0a74d5b96aa6d
Pradyun Narkadamilli <pradyungn@gmail.com>
2022-06-23 20:30:00 -0700

    Added metadata to 404, removed favicon

--- pages/404.js
+++ pages/404.js
@@ -3,12 +3,16 @@ import ThemeCtx from '../context/theme'
 
 import NavBar from '../components/navbar'
 import styles from '../styles/Error.module.css'
+import Meta from '../components/meta'
 
 export default function fourohfour() {
   const [theme, setTheme] = useContext(ThemeCtx)
 
   return (
     <main className={`container ${theme}`}>
+      <Meta siteTitle="Drifting in Space..."
+            description="A glimpse into the eternal void... an entity that does not exist."
+            img="red.webp"/>
       <NavBar/>
       <div className={styles.main}>
         <div className={styles.text}>
